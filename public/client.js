fcb361ee4acf19aa9df23c9576a629a1974e5110
Luiko <lokoluis_box@hotmail.com>
2017-06-09 23:57:52 -0600

    validation changed, now support ios

--- public/client.js
+++
@@ -1,6 +0,0 @@
-const summitInput = document.querySelector('#summitInput');
-const getSizeButton = document.querySelector('#getSizeButton');
-summitInput.addEventListener('change', function inputChange(e) {
-  const file = e.target.files[0];
-  if (file) getSizeButton.removeAttribute('disabled', false);
-});
